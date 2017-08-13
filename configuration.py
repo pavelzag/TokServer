227323a8d013ada2805e445fad1ea59dc792a710
pavelzagalsky <zagalsky@gmail.com>
2017-08-13 23:15:47 +0300

    Added heroku to config

--- configuration.py
+++ configuration.py
@@ -1,8 +1,16 @@
+import os
 import yaml
 
 
 def get_config(parameter_type, parameter_name):
-    with open("config.yml", 'r') as ymlfile:
-        cfg = yaml.load(ymlfile)
-    if parameter_type == 'db-params':
-        return cfg['db-params'][parameter_name]
+    if 'DYNO' in os.environ:
+        is_heroku = True
+    else:
+        is_heroku = False
+    if is_heroku:
+        return os.environ.get(parameter_name, 'Theres\'s nothing here')
+    else:
+        with open("config.yml", 'r') as ymlfile:
+            cfg = yaml.load(ymlfile)
+        if parameter_type == 'db-params':
+            return cfg['db-params'][parameter_name]
